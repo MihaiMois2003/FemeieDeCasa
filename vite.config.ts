b63a554ac8ea46ec04e00980e5c559087730b76b
MihaiMois2003 <mihaimoise73@gmail.com>
2025-05-20 22:29:33 +0300

    netlify fix try 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/FemeieDeCasa/", //repo name
+  base: "/", //repo name
   plugins: [react()],
 });
